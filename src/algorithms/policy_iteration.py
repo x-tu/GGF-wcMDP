bccb2961187f278ae6e9c2159404f924e29645e2
Xiaohui <timewarp@qq.com>
2023-08-10 13:47:39 -0400

    feat: add ggf-pi and update experiment results

--- src/algorithms/policy_iteration.py
+++ src/algorithms/policy_iteration.py
@@ -2,6 +2,12 @@
 
 import numpy as np
 
+# results for fixing the RL policy
+from solver.fix_policy import (
+    build_ggf_fix,
+    extract_results as extract_results_fix,
+    solve_ggf_fix,
+)
 from utils.mrp_lp import MRPData
 
 
@@ -19,6 +25,7 @@ class PIAgent:
         self.n_state = len(self.mrp_data.idx_list_s)
         self.n_action = len(self.mrp_data.idx_list_a)
         self.n_group = self.mrp_data.n_group
+        self.q_table = np.zeros((self.n_state, self.n_action), dtype=np.float64)
 
     def policy_evaluation(self, pi):
         if not self.ggi:
@@ -58,50 +65,37 @@ class PIAgent:
                     Q[s][a] += reward + self.gamma * np.dot(V, next_state_prob)
             new_pi = lambda s: {s: a for s, a in enumerate(np.argmax(Q, axis=1))}[s]
         else:
-            Qn = np.zeros(self.n_group, dtype=np.float64)
             for s in range(self.n_state):
                 for a in range(self.n_action):
                     next_state_prob = self.mrp_data.bigT[s, :, a]
                     reward = -self.mrp_data.bigC[s, a, :] + 110
-                    for g in range(self.n_group):
-                        Qn[g] += reward[g] + self.gamma * np.dot(
-                            V[:, g], next_state_prob
-                        )
-                    Q[s][a] = np.dot([1, 0.5, 0.25], np.sort(Qn))
+                    Qn = reward + self.gamma * np.dot(V.T, next_state_prob)
+                    Q[s][a] += np.dot([1, 0.5, 0.25], np.sort(Qn))
             new_pi = lambda s: {s: a for s, a in enumerate(np.argmax(Q, axis=1))}[s]
+        self.q_table = Q
         return new_pi
 
     def run_policy_iteration(self):
         # set initial actions to 0
         init_actions = [0 for s in range(self.n_state)]
         pi = lambda s: {s: 0 for s, a in enumerate(init_actions)}[s]
+        iteration = 0
+        rewards = []
         while True:
             old_pi = {s: pi(s) for s in range(self.n_state)}
             # TODO: add GGF policy evaluation and improvement
             Vs = self.policy_evaluation(pi)
             pi = self.policy_improvement(Vs)
+            iteration += 1
             # convergence check
-            if old_pi == {s: pi(s) for s in range(self.n_state)}:
+            new_pi = {s: pi(s) for s in range(self.n_state)}
+            if old_pi == new_pi:
                 break
+            model = build_ggf_fix(self.mrp_data, new_pi)
+            # Solve the GGF model
+            results, ggf_model = solve_ggf_fix(model=model)
+            reward = extract_results_fix(
+                model=ggf_model, data=self.mrp_data, policy_rl=new_pi
+            )
+            rewards.append(reward)
         return Vs, pi
-
-
-# initialize the environment
-params = {"n_group": 3, "n_state": 3, "n_action": 2, "ggi": True}
-# initialize the agent
-policy_agent = PIAgent(params, gamma=0.99, theta=1e-10)
-# run policy iteration
-V, pi = policy_agent.run_policy_iteration()
-policy = {s: pi(s) for s in range(policy_agent.n_state)}
-
-# results for fixing the RL policy
-from solver.fix_policy import (
-    build_ggf_fix,
-    extract_results as extract_results_fix,
-    solve_ggf_fix,
-)
-
-model = build_ggf_fix(policy_agent.mrp_data, policy)
-# Solve the GGF model
-results, ggf_model = solve_ggf_fix(model=model)
-extract_results_fix(model=ggf_model, data=policy_agent.mrp_data, policy_rl=policy)
--- src/algorithms/tabular_q.py
+++ src/algorithms/tabular_q.py
@@ -20,7 +20,7 @@ class QAgent:
         self.num_groups = num_groups
         self.ggi = env.ggi
         self.q_table = np.random.uniform(
-            low=5000, high=5100, size=(num_states, num_actions)
+            low=3000, high=3100, size=(num_states, num_actions)
         )
         self.q_table_visit_freq = np.zeros(shape=(num_states, num_actions))
         self.epsilon = epsilon
@@ -77,7 +77,7 @@ def run_tabular_q(
     )
     agent.lr_decay_schedule = np.linspace(alpha, 0, num_episodes)
     # Run 200 episodes
-    for episode in range(num_episodes):
+    for episode in range(1, num_episodes + 1):
         # Initialize environment
         state = env.reset()
         total_reward = 0
@@ -94,12 +94,14 @@ def run_tabular_q(
         # We use a decaying epsilon-greedy method
         if agent.epsilon > 0.001:
             agent.epsilon = agent.epsilon * agent.decaying_factor
-        agent.alpha = agent.lr_decay_schedule[episode]
+        agent.alpha = agent.lr_decay_schedule[episode - 1]
         # Display running rewards
         if episode % 20 == 0:
             print(
-                f"Episode: {episode}; Running reward: {total_reward:.1f}; "
-                f"Epsilon: {agent.epsilon:.1f}; Alpha: {agent.alpha:.1f}"
+                f"Episode: {episode}; "
+                f"Running Reward: {total_reward:.1f}; "
+                f"Epsilon: {agent.epsilon:.3f}; "
+                f"Learning Rate: {agent.alpha:.3f}"
             )
         episode_rewards.append(total_reward)
     state_action_pair = agent.get_policy()
@@ -108,3 +110,41 @@ def run_tabular_q(
     print("===== Q table visitation count =====")
     print(pd.DataFrame(agent.q_table_visit_freq))
     return state_action_pair, episode_rewards
+
+
+# run the script as main
+if __name__ == "__main__":
+    import argparse
+
+    import numpy as np
+    import seaborn as sns
+
+    from env.mrp_env import MachineReplace
+    from experiments.configs.config_shared import prs_parser_setup
+
+    # configuration parameters
+    prs = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="""Tabular Q Learning.""",
+    )
+    prs = prs_parser_setup(prs)
+    args, _ = prs.parse_known_args()
+
+    # initialize the MRP environment
+    env = MachineReplace(
+        n_group=args.n_group,
+        n_state=args.n_state,
+        n_action=args.n_action,
+        init_state=0,
+        ggi=args.ggi,
+    )
+
+    tabular_q_policy, episode_rewards = run_tabular_q(
+        env=env,
+        num_episodes=500,
+        len_episode=1500,
+        alpha=0.1,
+        epsilon=0.2,
+        decaying_factor=0.99,
+        gamma=0.90,
+    )
