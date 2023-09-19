12ca9ea5e6062610a46d737cf38083e582def012
Xiaohui <timewarp@qq.com>
2023-09-19 11:48:17 -0400

    refactor: remove unused files

--- src/experiments/archive/momdp.py
+++
@@ -1,9 +0,0 @@
-"""Example instances for solving MOMDP model."""
-
-from solver.momdp import solve_mrp
-from utils.mrp import MRPData
-
-# Get the MRP data
-mrp_data = MRPData(n_group=3, n_state=3, n_action=2, weight=[1, 0.5, 0.25])
-# Solve the MOMDP model
-results, momdp_model = solve_mrp(input_data=mrp_data)
