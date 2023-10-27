73901a344206c67206bc5a5c50239e0802622941
Xiaohui <timewarp@qq.com>
2023-10-27 14:34:55 -0400

    feat: add sample loop

--- src/experiments/tests/policy_simulator.py
+++ src/experiments/tests/policy_simulator.py
@@ -10,7 +10,7 @@ from env.mrp_env_rccc import MachineReplacement
 from experiments.configs.base import params
 
 # Notice: modify the file path to the one containing policy
-policy = pd.read_csv("../results/policy/1026/policy_dqnS_4.csv", index_col=False)
+policy = pd.read_csv("../results/policy/1026/policy_dqnS_4.csv")
 # drop the index column
 policy = policy.iloc[:, 1:]
 
@@ -36,11 +36,12 @@ action_size = policy.shape[1]
 episode_rewards = []
 for ep in range(params.num_episodes):
     # run LP
-    init_state = random.randint(0, state_size - 1)
-    total_reward = 0
-    for t in range(params.len_episode):
-        for init_state in range(state_size):
-            state = env.reset(initial_state=init_state)
+    ep_rewards = []
+    for n in range(params.num_samples):
+        init_state = random.randint(0, state_size - 1)
+        state = env.reset(initial_state=init_state)
+        total_reward = 0
+        for t in range(params.len_episode):
             action_prob = policy.iloc[state].tolist()
             action = random.choices(range(len(action_prob)), weights=action_prob, k=1)[
                 0
@@ -48,7 +49,9 @@ for ep in range(params.num_episodes):
             next_observation, reward, done, _ = env.step(action)
             total_reward += (1 - done) * params.gamma ** t * reward
             state = next_observation
-    rewards_sorted = np.sort(total_reward)
+        ep_rewards.append(total_reward)
+    # get the expected rewards by averaging over samples, and then sort
+    rewards_sorted = np.sort(np.mean(ep_rewards, axis=0))
     episode_rewards.append(np.dot(env.weights, rewards_sorted))
 sns.lineplot(episode_rewards)
 print(np.mean(episode_rewards))
