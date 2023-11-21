3d0056acfebec1d79222495da3348602bd9f23f4
Xiaohui <timewarp@qq.com>
2023-11-21 00:03:30 -0500

    feat: add cost type string supports & update base parameters

--- src/experiments/configs/base.py
+++ src/experiments/configs/base.py
@@ -4,17 +4,22 @@ from utils.common import DotDict
 
 params = DotDict(
     {
+        # general parameters for the MDP
         "num_actions": 2,
         "num_states": 3,
         "num_groups": 3,
         "ggi": True,
+        "rccc_wrt_max": 0.5,
+        "mat_type": 1,
+        "weight_type": "exponential2",
+        "cost_type_operation": "quadratic",
+        "cost_type_replace": "rccc",
+        "prob_remain": 0.8,
+        "num_opt_solutions": 1,
         "num_episodes": 1000,
         "len_episode": 100,
         "num_samples": 10,
         "gamma": 0.95,
-        "rccc_wrt_max": 0.5,
-        "mat_type": 1,
-        "prob_remain": 0.8,
         # hyperparameters for the Q-learning
         "ql": DotDict(
             {
