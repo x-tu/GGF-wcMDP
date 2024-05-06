69bd9443f4f2867a082aebd0322d61fba03172c9
x-tu <xiaohui.tu@hec.ca>
2024-05-06 18:24:39 -0400

    refactor: add bar plot, statistical tests for mean

--- experiments/test_whittle.py
+++ experiments/test_whittle.py
@@ -1,14 +1,17 @@
 import numpy as np
+import pandas as pd
+from scipy import stats
 
 from algorithms.whittle import Whittle
 from experiments.configs.base import params
-from utils.count import CountMDP
 from utils.mrp import MRPData
 
 params.num_states = 3
 params.num_groups = 2
+params.len_episode = 300
+budget = 1
+n_runs = 1000
 
-# count_mdp = CountMDP(num_groups=2, num_resource=1, num_states=3)
 mrp = MRPData(num_groups=params.num_groups, num_states=params.num_states)
 
 whittle_agent = Whittle(
@@ -16,30 +19,60 @@ whittle_agent = Whittle(
     num_arms=params.num_groups,
     reward=mrp.rewards + 1,  # (arm, x, act)
     transition=mrp.transitions,  # (arm, x, x', act)
-    horizon=300,
+    horizon=params.len_episode,
 )
-whittle_agent.whittle_brute_force(lower_bound=-1.1, upper_bound=1.1, num_trials=1000)
+whittle_agent.whittle_brute_force(lower_bound=-1.1, upper_bound=0.1, num_trials=1000)
+
 # MC simulation to evaluate the policy
-state = np.array([0] * params.num_groups)
-total_reward = 0
-for t in range(300):
-    # with 1 arm to be selected
-    action = whittle_agent.Whittle_policy(
-        whittle_indices=whittle_agent.w_indices,
-        n_selection=1,
-        current_x=state,
-        current_t=t,
-    )
-    # get the reward
-    next_state = np.zeros_like(state)
-    for arm in range(params.num_groups):
-        total_reward += (mrp.rewards[arm, state[arm], action[arm]] + 1) * (
-            params.gamma**t
+group_rewards = np.zeros((params.num_groups, n_runs))
+for run in range(n_runs):
+    s_idx = np.random.choice(len(mrp.global_states))
+    state = mrp.global_states[s_idx]
+    for t in range(params.len_episode):
+        action = whittle_agent.Whittle_policy(
+            whittle_indices=whittle_agent.w_indices,
+            n_selection=budget,
+            current_x=state,
+            current_t=t,
         )
-        next_state[arm] = np.random.choice(
-            range(params.num_states), p=mrp.transitions[arm, state[arm], :, action[arm]]
-        )
-    state = next_state
-print(
-    total_reward / params.num_groups
-)  # to get the mean group reward to compare with weighted average comparison
+        # get the reward
+        next_state = np.zeros_like(state)
+        for arm in range(params.num_groups):
+            group_rewards[arm, run] += (
+                mrp.rewards[arm, state[arm], action[arm]] + 1
+            ) * (params.gamma**t)
+            next_state[arm] = np.random.choice(
+                range(params.num_states),
+                p=mrp.transitions[arm, state[arm], :, action[arm]],
+            )
+        state = next_state
+# to get the mean group reward to compare with weighted average comparison
+total_rewards = group_rewards.sum(axis=0) / params.num_groups
+
+
+def check_equal_means(groups):
+    f_statistic, p_value = stats.f_oneway(*groups)
+    return p_value > 0.05
+
+
+print(check_equal_means(group_rewards))
+
+# plot histogram of the individual rewards
+import matplotlib.pyplot as plt
+
+rewards_df = pd.DataFrame(group_rewards.T)
+rewards_df.columns = [f"Group {i}" for i in range(params.num_groups)]
+# plot the mean and variance of the two groups
+rewards_df.mean().plot(kind="bar", yerr=rewards_df.std())
+# label is horizontal
+plt.xticks(rotation=0)
+plt.show()
+# plot the distribution of the differences of rewards
+differences = group_rewards[0] - group_rewards[1]
+# plot the histogram of the differences with density estimation
+plt.hist(differences, bins=50, alpha=0.5, density=True)
+# plt.bar(differences, stats.norm.pdf(differences, np.mean(differences), np.std(differences)))
+plt.show()
+
+# save the rewards
+rewards_df.to_csv("experiments/tmp/whittle_rewards.csv", index=False)
