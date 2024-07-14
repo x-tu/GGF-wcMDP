d49d9ae5d1eae7ed50b10bbdb21829aeb6763d60
x-tu <xiaohui.tu@hec.ca>
2024-07-14 17:46:35 -0400

    feat: plot learning curves

--- experiments/plot_figures.py
+++ experiments/plot_figures.py
@@ -2,21 +2,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from experiments.configs.base import params
 from utils.plots import moving_average
 
 data = {}
-for algorithm in ["random", "sac", "ddpg", "ppo", "td3"]:
-    df = pd.read_csv(f"experiments/tmp/{algorithm}.csv")["0"][:600]
+for algorithm in ["sac", "ppo", "td3"]:
+    df = pd.read_csv(
+        f"experiments/tmp/learning_reward_{algorithm}_{params.identifier}.csv"
+    )["0"]
     df = moving_average(df, window=10)
-    label = algorithm.upper() if algorithm != "random" else "Random"
+    label = algorithm.upper()
     data[label] = df
 data_df = pd.DataFrame(data)
-num_episodes = data_df.shape[0]
+num_episodes = int(np.ceil(data_df.shape[0] / 100) * 100)
+
+weights = np.array([1 / (2**i) for i in range(params.num_groups)])
+weights /= np.sum(weights)
+
+# random
+df = pd.read_csv(f"experiments/tmp/rewards_random_{params.identifier}.csv")
+# drop the first column
+df = df.drop(df.columns[0], axis=1)
+df_random = moving_average(
+    np.dot(np.sort(df), params.weights).round(params.digit), window=10
+)
+data_df["Random"] = df_random[0 : data_df.shape[0]]
 
 # Plot the data
 data_df.plot()
 # add whittle index
-whittle_policy = pd.read_csv("experiments/tmp/rewards_whittle5.csv")
+whittle_policy = pd.read_csv(f"experiments/tmp/rewards_whittle_{params.identifier}.csv")
 whittle_policy = whittle_policy.mean(axis=1)
 wt_mean = whittle_policy.mean()
 wt_std = whittle_policy.std() / np.sqrt(len(whittle_policy))
@@ -24,12 +39,15 @@ plt.axhline(y=wt_mean, color="r", linestyle="--", label="Whittle")
 plt.fill_between(
     range(num_episodes), wt_mean - wt_std, wt_mean + wt_std, color="r", alpha=0.1
 )
+ymax = np.ceil(data_df.max().max()) + 1
 
-plt.ylim([8, 18])
-plt.xlim([-5, 600])
+plt.ylim([8, ymax])
+plt.xlim([-5, num_episodes])
 plt.xlabel("Episodes")
-plt.ylabel("Mean Expected Returns")
+plt.ylabel("GGF Expected Returns")
 plt.title("Learning Curves (Smoothed)")
-# show the legend
+# set figure size
+plt.gcf().set_size_inches(6, 5)
+plt.savefig(f"experiments/tmp/{params.identifier}.png")
 plt.legend()
 plt.show()
