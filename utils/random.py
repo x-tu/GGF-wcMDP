f6e17144823a241dded0533142f15be7a1a8a5f7
x-tu <xiaohui.tu@hec.ca>
2024-07-14 17:44:26 -0400

    feat: run multiple times at each step to calculate GGF

--- utils/random.py
+++ utils/random.py
@@ -8,15 +8,20 @@ class RandomAgent:
     def __init__(self, env):
         self.env = env
 
-    def run(self, num_episodes=600, len_episode=300):
+    def run(self, num_episodes=600, len_episode=300, runs=10):
         ep_rewards = np.zeros((num_episodes, self.env.num_groups))
         for ep in tqdm(range(num_episodes)):
-            for t in range(len_episode):
-                action_priority = np.array(
-                    [1 / self.env.count_mdp.num_states] * self.env.count_mdp.num_states
-                )
-                if not self.env.force_to_use_all_resources:
-                    action_priority += [np.random.uniform(0, 1)]
-                _, _, _, _ = self.env.step(action_priority)
-            ep_rewards[ep, :] = self.env.last_group_rewards
+            run_rewards = np.zeros((runs, self.env.num_groups))
+            for run in range(runs):
+                _ = self.env.reset()
+                for t in range(len_episode):
+                    action_priority = np.array(
+                        [1 / self.env.count_mdp.num_states]
+                        * self.env.count_mdp.num_states
+                    )
+                    if not self.env.force_to_use_all_resources:
+                        action_priority += [np.random.uniform(0, 1)]
+                    _, _, _, _ = self.env.step(action_priority)
+                run_rewards[run, :] = self.env.last_group_rewards
+            ep_rewards[ep, :] = run_rewards.mean(axis=0)
         return ep_rewards
