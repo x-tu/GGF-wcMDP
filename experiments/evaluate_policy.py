0ca4aa21c70a99f5a0e2b67c2ec0de6f273d933f
x-tu <xiaohui.tu@hec.ca>
2024-07-13 23:51:49 -0400

    fix: constant params in upper case; rename to distinguish local and global vars

--- experiments/evaluate_policy.py
+++ experiments/evaluate_policy.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -6,16 +5,16 @@ from tqdm import tqdm
 
 from env.mrp_simulation import PropCountSimMDPEnv
 from experiments.configs.base import params
-from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
+from stable_baselines3 import PPO, SAC, TD3
 from utils.count import CountMDP
 from utils.policy import check_equal_means
 
-algorithms = [PPO, SAC, TD3]
-runs = 10
-file_out = False
+ALGORITHMS = [PPO, SAC, TD3]
+RUNS = 1000
+FILE_OUT = False
 
 
-def extract_policy(env, model):
+def extract_policy(env_policy, model_policy):
     count_mdp = CountMDP(
         num_groups=params.num_groups,
         num_resource=params.budget,
@@ -24,9 +23,9 @@ def extract_policy(env, model):
     # Print the policy
     for state in count_mdp.count_state_props:
         th_obs = torch.as_tensor(state).unsqueeze(0)
-        env.observations = state
-        action_priority, _ = model.predict(state, deterministic=True)
-        count_action = env.select_action_by_priority(action_priority)
+        env_policy.observations = state
+        action_priority, _ = model_policy.predict(state, deterministic=True)
+        count_action = env_policy.select_action_by_priority(action_priority)
         print(
             "state",
             state * params.num_groups,
@@ -37,23 +36,23 @@ def extract_policy(env, model):
         )
 
 
-def simulate_group_rewards(env, model, runs):
-    group_rewards = np.zeros((runs, params.num_groups))
+def simulate_group_rewards(env_sim, model_sim, runs):
+    rewards = np.zeros((runs, params.num_groups))
     for run in tqdm(range(runs)):
-        state = env.reset()
+        state = env_sim.reset()
         for t in range(params.len_episode):
             # get the action
-            action_priority, _ = model.predict(state, deterministic=True)
-            next_state, reward, done, info = env.step(action_priority)
+            action_priority, _ = model_sim.predict(state, deterministic=True)
+            next_state, reward, done, info = env_sim.step(action_priority)
             state = next_state
-        group_rewards[run, :] = env.last_group_rewards
-    print("Mean:", group_rewards.mean(axis=0).round(params.digit))
-    print("Std:", group_rewards.std(axis=0).round(params.digit))
-    return group_rewards
+        rewards[run, :] = env_sim.last_group_rewards
+    print("Mean:", rewards.mean(axis=0).round(params.digit))
+    print("Std:", rewards.std(axis=0).round(params.digit))
+    return rewards
 
 
 # Evaluate the policy
-for algorithm in algorithms:
+for algorithm in ALGORITHMS:
     env = PropCountSimMDPEnv(
         machine_range=[params.num_groups, params.num_groups],
         resource_range=[params.budget, params.budget],
@@ -67,9 +66,9 @@ for algorithm in algorithms:
         f"experiments/tmp/{algorithm.__name__.lower()}_{params.identifier}.zip"
     )
     # extract_policy(env, model)
-    group_rewards = simulate_group_rewards(env, model, runs=runs)
+    group_rewards = simulate_group_rewards(env, model, RUNS)
     rewards_df = pd.DataFrame(group_rewards)
-    if file_out:
+    if FILE_OUT:
         rewards_df.to_csv(
             f"experiments/tmp/rewards_{algorithm.__name__.lower()}_{params.identifier}.csv"
         )
