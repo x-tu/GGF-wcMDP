65f51cb3d98aab88a5deacd7d1f3b59984af5251
x-tu <xiaohui.tu@hec.ca>
2024-07-18 02:43:47 -0400

    feat: scale run might be problematic, test it

---
+++ experiments/scale_run_optimality.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from experiments.batch_run_optimality import train_agent
+from experiments.configs.base import params
+from stable_baselines3 import A2C, PPO, SAC, TD3
+from utils.plots import moving_average
+
+params.update({"num_episodes": 1000, "len_episode": 300})
+FILE_OUT = False
+
+model, training_rewards = train_agent(PPO, params)
+file_name = f"experiments/tmp/learning_reward_ppo_{params.identifier}.csv"
+if FILE_OUT:
+    pd.DataFrame(training_rewards).to_csv(file_name)
+plt.plot(moving_average(training_rewards, window=10), label="PPO")
+
+# plot figures
+plt.legend()
+plt.xlabel("Episodes")
+plt.ylabel("GGF Expected Returns")
+plt.title("Learning Curves (Smoothed)")
+plt.savefig(f"experiments/tmp/learning_reward_{params.identifier}.png")
+plt.show()
